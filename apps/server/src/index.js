7e7d59f23afde76c414996acc5a6f3c9ae433e60
Örjan Sjöholm <orjansjoholm@gmail.com>
2024-05-08 22:40:03 +0200

    Added simple long running node process with a build dependency

---
+++ apps/server/src/index.js
@@ -0,0 +1,18 @@
+const sleep = (ms) => {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+const applicationVersion = "v1.0.0";
+
+async function main() {
+    let iteration = 0;
+    while (true) {
+        iteration++;
+        console.log("Server is running", applicationVersion, iteration);
+        await sleep(5000);
+    }
+}
+
+(async () => {
+    await main();
+})();
